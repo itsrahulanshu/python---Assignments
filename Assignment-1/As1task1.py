7ef9c1788f8535a24c4e21dd7b2b597e7214d54e
itsrahulanshu <150064806+itsrahulanshu@users.noreply.github.com>
2025-03-13 19:06:45 +0530

    Add files via upload

---
+++ Assignment-1/As1task1.py
@@ -0,0 +1,10 @@
+a = int(input( "Enter the first number: "))
+b = int(input( "Enter the second number: "))
+add = a+b
+sub = a-b
+multi = a*b
+divi = a/b
+print("addition:",add)
+print("subtraction:",sub)
+print("multiplication:",multi)
+print("Division:",divi)
---
+++ Assignment-1/As1task2.py
@@ -0,0 +1,4 @@
+fn =input("Enter your first name: ")
+sn =input("input your second name: ")
+print("Hello,",fn+ " " + sn, "! Welcome to the python program.")
+
